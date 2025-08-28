02506db9220dd6e92aab9902fa1010099866ec1c
adeoye@APA-PC <adeoyejames2005@gmail.com>
2025-08-28 07:03:37 +0100

    first commit

--- firstCode.py
+++
@@ -1,4 +0,0 @@
-given_string=("Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor. diam et labore?"
-              " et diam magna. et diam amet.")
-given_string = given_string.split()
-print(type(given_string))
